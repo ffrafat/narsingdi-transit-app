0bb190248ab5de91371cf2868d14686951eb7025
ffrafat <faisalfaruquerafat@gmail.com>
2026-01-23 23:41:01 +0600

    emni

--- constants/heroThemes.js
+++ constants/heroThemes.js
@@ -1,21 +1,21 @@
 export const HERO_THEMES = [
     {
         id: 'default',
-        name: 'Default',
+        name: 'বাষ্পশকট',
         image: require('../assets/hero/default.jpg'),
         colors: ['#064e3b', '#059669'],
         statusBar: '#064e3b',
     },
     {
         id: 'islamic',
-        name: 'Islamic',
+        name: 'ইসলামিক',
         image: require('../assets/hero/islamic.jpg'),
         colors: ['#064e3b', '#059669'],
         statusBar: '#064e3b',
     },
     {
         id: 'abstract',
-        name: 'Abstract',
+        name: 'এলোমেলো',
         image: require('../assets/hero/abstract.jpg'),
         colors: ['#064e3b', '#059669'],
         statusBar: '#064e3b',
